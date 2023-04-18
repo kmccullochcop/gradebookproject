9bc7ff95673a9f6dcc254e19bd54c103a4af7552
Kathleen McCulloch-Cop <kathleenmcculloch-cop@Kathleens-MacBook-Air.local>
2023-04-18 09:53:20 -0400

    further templating

--- gradebook/src/gradebook/Course.java
+++ gradebook/src/gradebook/Course.java
@@ -14,28 +14,58 @@ public class Course {
 	//Level 4 => use ArrayList for marks rather than a set array
 	int[] marks = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	
+	/** Course
+	 *  creates new Course object
+	 *  @param: course code (String), year of course (int), semester of course (int)
+	 *  @return: void
+	**/
 	public Course(String inpCode, int inpYear, int inpSem){
 		code = inpCode;
 		sem = inpSem;
 		year = inpYear;
 	}
 	
+	/** getSem
+	 *  returns the semester of the course
+	 *  @param: null
+	 *  @return: void
+	**/
 	public int getSem() {
 		return sem;
 	}
 	
+	/** getYear
+	 *  returns the year the course was taken
+	 *  @param: null
+	 *  @return: int
+	**/
 	public int getYear() {
 		return year;
 	}
 	
+	/** getCode
+	 *  returns course code 
+	 *  @param: null
+	 *  @return: String
+	**/
 	public String getCode() {
 		return code;
 	}
 	
+	/** getMarks
+	 *  returns ArrayList of marks for the course
+	 *  @param: null
+	 *  @return: int[]
+	**/
 	public int[] getMarks() {
 		return marks;
 	}
 	
+	/** setMark
+	 *  adds new mark at given index
+	 *  @param: new mark (int), index (int)
+	 *  @return: void
+	**/
 	public void setMark(int inpMark, int index) {
 		if(index < marks.length) {
 			marks[index] = inpMark;
@@ -45,10 +75,20 @@ public class Course {
 	
 	//level 4 method
 	//have to use marks as array lists
+	/** addMark
+	 *  adds new mark to marks ArrayList
+	 *  @param: new mark (int)
+	 *  @return: void
+	**/
 	public void addMark(int mark) {
 		return;
 	}
 	
+	/** avg
+	 *  returns average mark for course 
+	 *  @param: null
+	 *  @return: double
+	**/
 	public double avg() {
 		int avg = 0;
 		for(int x : marks) {
